578f2400cb1a20732793cbd2c0b44ab6210be235
Tomi <tomi.taskila@tyomaa.com>
2021-08-11 16:38:30 +0300

    7.5: anecdotes and hooks step2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -90,6 +90,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -106,7 +113,8 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
